16545e68e16f4d2b1b23fe910370e88ce0a0f3da
sosuu <c0b23062ae@edu.teu.ac.jp>
2024-06-25 15:46:31 +0900

    念のためコミット

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -162,7 +162,6 @@ def main():
 
         for bomb in bombs:
             if bird.rct.colliderect(bomb.rct):
-                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 fonto = pg.font.Font(None, 80)
                 txt = fonto.render("Game Over", True, (255, 0, 0))
                 screen.blit(txt, [WIDTH/2-150, HEIGHT/2])
