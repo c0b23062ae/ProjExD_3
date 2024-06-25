4c7a1f2a84fb69ce36a20ca7076edcaa2ade0a03
sosuu <c0b23062ae@edu.teu.ac.jp>
2024-06-25 17:32:27 +0900

    追加機能２：途中

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -165,12 +165,14 @@ def main():
     clock = pg.time.Clock()
     game_score = Score()
     tmr = 0
+    multibeam = []
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE: # スペースキー押下でBeamクラスのインスタンス生成
-                    beam = Beam(bird)            
+                    beam = Beam(bird)
+                    multibeam.append()            
         screen.blit(bg_img, [0, 0])
 
 
